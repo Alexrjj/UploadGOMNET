87ad4de46824b8050d7986d32fd678131e2c7b50
Alex Piter <alexsoares2517@gmail.com>
2017-12-04 12:19:11 -0200

    reconhece a sob direto da pasta, descarta criação da 'sob.txt'

--- upload.py
+++ upload.py
@@ -2,8 +2,9 @@
 import os
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
-import openpyxl
+import openpyxl  #  Acessar os dados de login
 
+dirListing = os.listdir("./")
 #  Acessa os dados de login fora do script, salvo numa planilha existente, para proteger as informações de credenciais
 dados = openpyxl.load_workbook('C:\\gomnet.xlsx')
 login = dados['Plan1']
@@ -11,6 +12,7 @@ url = 'http://gomnet.ampla.com/'
 url2 = 'http://gomnet.ampla.com/Upload.aspx?numsob='
 username = login['A1'].value
 password = login['A2'].value
+
 
 driver = webdriver.Chrome()
 if __name__ == '__main__':
@@ -23,23 +25,23 @@ if __name__ == '__main__':
     submit_button = driver.find_element_by_id('ImageButton_Login').click()
 
     # Modifica os campos necessários e envia o anexo de cada sob contido nos arquivos txt.
-    with open('sobs.txt') as sobs:
-        for sob in sobs:
-            sob = sob.strip()
-            driver.get(url2 + sob.partition("_")[0])
+    # with open('sobs.txt') as sobs:
+    for item in dirListing:
+        if ".PDF" in item:
+            driver.get(url2 + item.partition("_")[0])
             try:  # Verifica se a sob foi digitada incorretamente.
                 erro = driver.find_element_by_xpath('*//tr/td[contains(text(),'
                                                     '"Não existem dados para serem exibidos.")]')
                 if erro.is_displayed():
-                    print("Sob " + sob.partition("_")[0] + " não encontrada. Favor verificar.")
+                    print("Sob " + item.partition("_")[0] + " não encontrada. Favor verificar.")
             except NoSuchElementException:
                 try:  # Verifica se o arquivo já foi anexado.
                     anexo = driver.find_element_by_xpath(
-                        "*//a[contains(text(), '" + sob + ".PDF""')]")
+                        "*//a[contains(text(), '" + item + "')]")
                     if anexo.is_displayed():
-                        print("Arquivo " + sob + ".PDF já foi anexado.")
+                        print("Arquivo " + item + " já foi anexado.")
                 except NoSuchElementException:
-                    # Preenche o campo "Descrição" com "PONTO DE SERVIÇO"
+                    # Preenche o campo "Descrição" com "PLANEJAMENTO"
                     atividade = driver.find_element_by_id('txtBoxDescricao')
                     atividade.send_keys('PONTO DE SERVIÇO')
                     # Identifica o menu " Categoria de Documento" e seleciona a opção "EXECUCAO"
@@ -48,17 +50,16 @@ if __name__ == '__main__':
                     # Identifica o menu " Tipo de Documento" e seleciona a opção "OUTROS"
                     documento = Select(driver.find_element_by_id('DropDownList1'))
                     documento.select_by_visible_text('OUTROS')
-                    driver.find_element_by_id('fileUPArquivo').send_keys(os.getcwd() + "\\" + sob + ".PDF")
+                    driver.find_element_by_id('fileUPArquivo').send_keys(os.getcwd() + "\\" + item)
                     driver.find_element_by_id('Button_Anexar').click()
                     try:
-                        # Verifica se a sob está no status desejado
-                        status = driver.find_element_by_xpath('//*[@id="txtBoxMessage"][contains(text(),'
-                                                              '"Arquivo salvo com sucesso.")]')
+                        # Verifica se o arquivo foi anexado com êxito
+                        status = driver.find_element_by_xpath("*//a[contains(text(), '" + item + "')]")
                         if status.is_displayed():
-                            print(sob + " anexado com sucesso.")
-                            driver.save_screenshot(sob + ".png")
+                            print("Arquivo " + item + " anexado com sucesso.")
                     except NoSuchElementException:
                         log = open("log.txt", "a")
-                        log.write(sob + " não foi anexado. \n")
+                        log.write(item + " não foi anexado.")
                         log.close()
                         continue
+    print("Fim da execução.")
